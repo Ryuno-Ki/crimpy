010b08d1a536ad7f2498623d7b771797991c8b83
André Jaenisch <andre.jaenisch@posteo.de>
2020-05-29 23:26:59 +0200

    Hello World

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.text = QLabel("Qt for Python!")
+        self.text.setAlignment(Qt.AlignCenter)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.setLayout(self.layout)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
